59839aab5cfdb75dccd868f82f08801351fdddfc
[lee sampson] <[emailme@leesampson.co.nz]>
2021-04-24 03:19:36 +1200

    complete scrolll to top and contact form and link to contact form need to trouble shoot scroll to top

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 // jshint esversion:6
 
-const express = require('express');
+// alert('click me for h1 magic');
+// $('h1').css('color', 'red');
 
-const app = express();
+// const express = require('express');
+// const app = express();
+
+// scroll to top code working on desktop not on localhost
+
+var mybutton = document.getElementById('myBtn');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE OPera
+}
+
+
+// above code is for scroll to top 
 
 app.use(express.static(__dirname + '/'));
 
@@ -10,6 +36,6 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + 'images');
 });
 
-app.listen(3000, function () {
-  console.log('Server has started on port 3000');
+app.listen(12345, function () {
+  console.log('Server has started on port 12345');
 });
